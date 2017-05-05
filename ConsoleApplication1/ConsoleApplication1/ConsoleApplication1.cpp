de4140e096b16ad3bbe09eee658cccb52e06dcaa
EricLIN <eric888a@hotmail.com.tw>
2017-05-05 23:34:17 +0800

    code V.2 std area

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -4,21 +4,20 @@
 #include "stdafx.h"
 
 
-using namespace std;
+//using namespace std;
 
 int main()
 {
-    string t[4] = { "aa", "bb", "cc", "dd" };
-    cout << "sizeof string: " << sizeof(string) << endl;
-    cout << "sizeof string t[4]: " << sizeof(t) << endl;
-    cout << "now start swapping t[0] and t[2]" << endl;
-    char *tmp;
-    tmp = (char*)malloc(sizeof(string));
-    memcpy(tmp, &t[0], sizeof(string));
-    memcpy(&t[0], &t[2], sizeof(string));
-    memcpy(&t[2], tmp, sizeof(string));
-    cout << "swap done!" << endl;
-    cout << "now the value of t[0] and t[2] is " << t[0] << " and " << t[2] << endl;
+    std::string t[4] = { "aa", "bb", "cc", "dd" };
+    std::cout << "sizeof string: " << sizeof(std::string) << std::endl;
+    std::cout << "sizeof string t[4]: " << sizeof(t) << std::endl;
+    std::cout << "now start swapping t[0] and t[2]" << std::endl;
+    auto tmp = static_cast<char*>(malloc(sizeof(std::string)));
+    memcpy(tmp, &t[0], sizeof(std::string));
+    memcpy(&t[0], &t[2], sizeof(std::string));
+    memcpy(&t[2], tmp, sizeof(std::string));
+    std::cout << "swap done!" << std::endl;
+    std::cout << "now the value of t[0] and t[2] is " << t[0] << " and " << t[2] << std::endl;
     return 0;
 }
 
